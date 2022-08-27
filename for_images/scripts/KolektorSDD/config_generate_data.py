94ae8e46bd133e649bf1d66776cfe3e3a5a2fae9
Mark Peng <markpeng@exosite.com>
2022-08-27 15:39:37 +0800

    Updated config.

--- for_images/scripts/KolektorSDD/config_generate_data.py
+++ for_images/scripts/KolektorSDD/config_generate_data.py
@@ -9,7 +9,7 @@ config = {
     },
 
     'global_config': {
-        'batch_size': 16,
+        'batch_size': 64,
         'z_dim': 100,
 
         'mg': 256,
@@ -19,34 +19,35 @@ config = {
         'disc_lr': 0.0002,
         'disc_beta1': 0.5,
 
-        'extra_iteration_checkpoint_freq': 50000,
-        'iteration_log_freq': 50000,
+        'extra_iteration_checkpoint_freq': 5000,
+        'iteration_log_freq': 5000,
         'visualization_freq': 200,
         'metric_freq': 400,
 
         'class_loss_with_fake': False,
         'bal_class_weights': False,
 
-        'num_generated_samples': 399,
+        'num_generated_samples': 500000,
     },
 
     'test_config': [
         {
             'method': ['raregan'],
             'dataset': ['KolektorSDD'],
-            'bgt': [10000],
-            'data_high_frc': [0.1],
+            'bgt': [1000],
+            'data_high_frc': [0.01],
             'run': [0],
 
-            'ini_rnd_bgt': [50],
-            'bgt_per_step': [50],
+            'ini_rnd_bgt': [500],
+            'bgt_per_step': [500],
 
             'high_frc_mul': [3.0],
 
             'bal_disc_weights': [True],
-            'num_iters_per_step': [10000],
+            'num_iters_per_step': [15000],
             'disc_disc_coe': [1.0],
             'gen_disc_coe': [1.0],
         },
+        {
     ]
 }
